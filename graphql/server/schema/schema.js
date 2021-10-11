cc88e07a2cce6c8b44b469cff8dd576fcaf04dd4
cyberspacedk <frontend.dk@gmail.com>
2021-10-11 09:18:29 +0300

    add new mutations

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -17,32 +17,6 @@ const {
   GraphQLNonNull
 } = graphql;
 
-//dummy data
-const usersData = [
-  { id: '1', name: 'Bond', age: 36, profession: 'Programmer' },
-  { id: '13', name: 'Anna', age: 26, profession: 'Baker' },
-  { id: '211', name: 'Bella', age: 16, profession: 'Mechanic' },
-  { id: '19', name: 'Gina', age: 26, profession: 'Painter' },
-  { id: '150', name: 'Georgina', age: 36, profession: 'Teacher' }
-];
-
-const hobbiesData = [
-  { id: '1', title: 'Programming', description: 'Using computers to make the world a better place', userId: '150' },
-  { id: '2', title: 'Rowing', description: 'Sweat and feel better before eating donouts', userId: '211' },
-  { id: '3', title: 'Swimming', description: 'Get in the water and learn to become the water', userId: '211' },
-  { id: '4', title: 'Fencing', description: 'A hobby for fency people', userId: '13' },
-  { id: '5', title: 'Hiking', description: 'Wear hiking boots and explore the world', userId: '150' },
-];
-
-const postsData = [
-  { id: '1', comment: 'Building a Mind', userId: '1' },
-  { id: '2', comment: 'GraphQL is Amazing', userId: '1' },
-  { id: '3', comment: 'How to Change the World', userId: '19' },
-  { id: '4', comment: 'How to Change the World', userId: '211' },
-  { id: '5', comment: 'How to Change the World', userId: '1' }
-]
-
-
 const UserType = new GraphQLObjectType({
   name: 'User',
   description: 'Documentation for user',
@@ -75,6 +49,7 @@ const HobbyType = new GraphQLObjectType({
     id: { type: GraphQLID },
     title: { type: GraphQLString },
     description: { type: GraphQLString },
+    userId: { type: GraphQLID },
     user: {
       type: UserType,
       resolve(parent) {
@@ -91,6 +66,7 @@ const PostType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     comment: { type: GraphQLString },
+    userId: { type: GraphQLID },
     user: {
       type: UserType,
       resolve(parent, args) {
@@ -179,14 +155,12 @@ const Mutation = new GraphQLObjectType({
         profession: { type: new GraphQLNonNull(GraphQLString) }
       },
       resolve(parent, args) {
-        // Create new User invoking mongoose model
         const user = new User({
           name: args.name,
           age: args.age,
           profession: args.profession
         });
 
-        // save to DB
         user.save();
 
         return user;
@@ -196,19 +170,17 @@ const Mutation = new GraphQLObjectType({
     updateUser: {
       type: UserType,
       args: {
-        id: new GraphQLNonNull(GraphQLID),
-        name: { type: GraphQLString },
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
         age: { type: GraphQLInt },
         profession: { type: GraphQLString }
       },
       resolve(parent, args) {
-        const data = {
-          ...(args.name && { name: args.name }),
-          ...(args.age && { name: args.age }),
-          ...(args.profession && { name: args.profession }),
-
-        };
-        return User.findByIdAndUpdate(args.id, data)
+        return User.findByIdAndUpdate(args.id, {
+          name: args.name,
+          age: args.age,
+          profession: args.profession
+        }, { new: true });
       }
     },
 
@@ -230,6 +202,18 @@ const Mutation = new GraphQLObjectType({
       }
     },
 
+    updatePost: {
+      type: PostType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        comment: { type: new GraphQLNonNull(GraphQLString) },
+        userId: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        return Post.findByIdAndUpdate(args.id, { comment: args.comment, userId: args.userId }, { new: true })
+      }
+    },
+
     createHobby: {
       type: HobbyType,
       args: {
@@ -247,6 +231,23 @@ const Mutation = new GraphQLObjectType({
         hobby.save();
 
         return hobby;
+      }
+    },
+
+    updateHobby: {
+      type: HobbyType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        userId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return Hobby.findByIdAndUpdate(args.id, {
+          title: args.title,
+          description: args.description,
+          userId: args.userId,
+        }, { new: true })
       }
     }
   }
