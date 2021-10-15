d9b6cdeb16436ac86e60edb1600780c775076101
cyberspacedk <frontend.dk@gmail.com>
2021-10-15 08:58:35 +0300

    add S3

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import {withAuthenticator} from 'aws-amplify-react';
 
 import DisplayPosts from './components/DisplayPosts';
 import CreatePost from './components/CreatePost';
+import AddFileToS3 from './components/AddFileToS3'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AddFileToS3 />
       <CreatePost />
       <DisplayPosts />
     </div>
---
+++ client/src/components/AddFileToS3.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Storage} from 'aws-amplify';
+
+const AddFileToS3 = () => {
+
+  const putHandler = async () => {
+    const res = await Storage.put('text.js', 'alert("Hello")');
+    console.log("🚀 ~ ", res)
+  }
+
+  return (
+    <div>
+        <button onClick={putHandler}>Add to S3</button>
+    </div>
+  )
+}
+
+export default AddFileToS3;
